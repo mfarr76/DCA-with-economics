2c7f8a9d84a6c3545ebb74b979929b54b234a51a
mfarr76 <mfarr76@gmail.com>
2017-07-05 16:39:54 -0500

    update - 7/5/17

--- purrr_iteration.R
+++ purrr_iteration.R
@@ -1,4 +1,3 @@
-rm(list = ls())
 
 rm(list = ls())
 load("C:/Users/MFARR/Documents/R_files/Spotfire.data/cashflow.RData")
@@ -10,55 +9,45 @@ gPrice <- seq(1, 5, by = 0.5)
 oPrice <- seq(30, 70, by = 5)
 #nPrice <- c(5, 10, 20, 30, 40)
 nPrice <- oPrice *0.4
-gPrice[1]
+price <- cbind(gPrice, oPrice, nPrice)
 
+gas_price[1]
 
-mapModel <- data.frame(map(1:5, cfmodel))
-
-cfmodel1 <- function(x){
-  data.frame(Scenario = (Cashflow$net_gas.mcf * gPrice[x]
-                         + Cashflow$net_oil.bbl * oPrice[x] + Cashflow$net_ngl.bbl * nPrice)
-             - opex)
-}
-
-cfmodel2 <- function(x){
-  data.frame(Scenario = (NCF.tbl$NetDryGas * gPrice[x]
-                         + NCF.tbl$NetOil * oPrice[x] + NCF.tbl$NetNGL * nPrice) 
-             - opex)
-}
-
-
-
-cfmodel1(1)
-
-(mapModel1 <- data.frame(map(1:4, cfmodel1)))
-(mapModel2 <- data.frame(map(1:9, cfmodel2)))
-
-(new.CFmodel <- bind_cols(NCF.tbl, mapModel2))
 ##------------------------------------
 
 ###unique well inputs will have the same order...nglyield, shrink, wi, nri, capex, opex 
 
-Well1 <- c(100, 0.8, 1, 0.75, 5000, 1000)
-well2 <- c(100, 0.8, 1, 0.75, 5000, 1000)
 
 
-###cashflow sensitivity with purrr
-NCF.tbl <- TypeCurve %>%
-  mutate(Time = as.numeric(Time),
-         NetDryGas = as.numeric(Gas.mcf * shrink * NRI * work_int),
-         NetOil = as.numeric(Oil.bbl * NRI * work_int),
-         NetNGL = as.numeric(Gas.mcf/1000 * ngl_yield * NRI * work_int)) %>%
-  select(Time, NetDryGas, NetOil, NetNGL)
-
-
-cfmodel <- function(x){
-  data.frame(Scenario = (NCF.tbl$NetDryGas * gPrice[x]
-                         + NCF.tbl$NetOil * oPrice[x] + NCF.tbl$NetNGL * nPrice) 
-             - opex)
+cashflow_sensitivity <- function(x)
+{
+  x <- data.frame(Sensitivity = (cf_model$NetDryGas * gPrice[x]
+                         + cf_model$NetOil * oPrice[x] + cf_model$NetNGL * nPrice[x]))
+  x$Name <- cf_model$TCName
+  
+  return(x)
 }
 
-purrrModel <- data.frame(map(1:9, cfmodel))
+cf_sens(3)
+
+
+purrr_out <- data.frame()
+for(i in 1:length(wellnames)){
+  
+  cf_reduced <- subset(cf_model %>% select(TCName, Time, NetDryGas, NetOil, NetNGL),
+                     TCName == wellnames[i])
+  inputs_reduced <- subset(inputs %>% select(Name, capex, opex),
+                           Name == wellnames[i])
+  
+  
+  purrrModel <- data.frame(map(1:9, cashflow_cashflow_sensitivity(inputs_reduced)))
+  purrr_out <- cbind(purrr_out, purrrModel)
+  
+}
+
+
+
+purrrModel <- data.frame(map(1:9, cf_sens))
 
 CFmodel <- bind_cols(NCF.tbl, purrrModel)
 
@@ -68,8 +57,10 @@ CFmodel <- bind_cols(NCF.tbl, purrrModel)
 
 rm(list = ls())
 load("C:/Users/MFARR/Documents/R_files/Spotfire.data/cashflow.RData")
+#load("C:/Users/MFARR/Documents/R_files/Spotfire.data/tcjoin.RData")
 #install.packages("purrr")
 #library(data.table)
+library(dplyr)
 library(purrr)
 
 TCGroups %>%
@@ -90,40 +81,19 @@ capex_mnth <- 1
 #work_int <- 1
 #NRI <- .75
 discRateSingle <- 0.15
+
+
 wellnames <- unique(TCGroups$Name)
-
-
-well1 <- data.frame(Name = wellnames[1], nglyield = 100, shrink = 0.8, wi = 1, nri = 0.75, capex = 5000, opex = 1000, stringsAsFactors = FALSE)
-well2 <- data.frame(Name = wellnames[2], nglyield = 1, shrink = 1, wi = 1, nri = 1, capex = 10, opex = 10, stringsAsFactors = FALSE)
+well1 <- data.frame(Name = wellnames[1], ngl.yield = 100, shrink = 0.8, 
+                    wi = 1, nri = 0.75, capex = 5000, opex = 1000, discount.rate = 0.15 ,stringsAsFactors = FALSE)
+well2 <- data.frame(Name = wellnames[2], ngl.yield = 1, shrink = 1, 
+                    wi = 1, nri = 1, capex = 10, opex = 10, discount.rate = 0.15, stringsAsFactors = FALSE)
 inputs <- rbind(well1, well2)
 
 
 
 
-
-out <- data.frame()
-intake <- data.frame()
-modelout<- data.frame()
-for(i in 1:length(wellnames))
-{ 
-  
-  intake <- inputs[i, 1:ncol(inputs)]
-  
-  
-  out <- subset(TCGroups, Name == wellnames[i])
-  
-  
-  model <- cshflw_redo(out, intake)
-  modelout <- rbind(modelout, model)
-  
-}
-
-
-
-tc <- TCGroups
-
-
-cshflw_redo <- function(tc, inputs)
+cshflow <- function(tc, inputs)
 {
   TCName = tc$Name
   Time = as.numeric(tc$Time)
@@ -137,32 +107,78 @@ cshflw_redo <- function(tc, inputs)
   OilRev = NetOil * oil_price
   NGLRev = NetNGL * ngl_price
   NetRev = GasRev + OilRev + NGLRev
-  OpIncome = ifelse(NetRev - inputs$opex < 0, 0 ,NetRev - inputs$opex)
-  OpIncome1 = NetRev - inputs$opex
+  OpIncome = NetRev - inputs$opex
   Undisc.CF = OpIncome - ifelse(Time == capex_mnth, inputs$capex * 1000, 0)
   Disc.Capex = (ifelse(Time == capex_mnth, (1/(1 + discRateSingle)^((capex_mnth - 1)/12))*inputs$capex*1000, 0))
   Disc.CF = as.numeric(OpIncome *(1/(1 + discRateSingle)^((Time - 0.5) / 12)) - Disc.Capex)
   
-  results <- data.frame(TCName, Time, GRGas.mcf, GROil.bbl, GRNgl.bbl, NetDryGas, NetOil, NetNGL, GasRev, OilRev, NGLRev, NetRev,OpIncome, OpIncome1, Undisc.CF, 
-                        Disc.Capex, Disc.CF)
-  #results <- filter(results, OpIncome > 0) ###REMEMBER TO TURN THIS BACK ON...OFF FOR TESTING
+  results <- data.frame(TCName, Time, GRGas.mcf, GROil.bbl, GRNgl.bbl, NetDryGas, NetOil, NetNGL,
+                        GasRev, OilRev, NGLRev, NetRev, OpIncome, Undisc.CF, Disc.Capex, Disc.CF)
   
-  #results$Scenario <- Scenario
-  
+  results <- filter(results, OpIncome > 0)
   
   return(results)
 }
 
-cshflw_redo(TCGroups, inputs)
+
+#xcash <- cshflow(TCGroups, inputs)
 
 
+
+#-------------------for loop to calc F11 output for all TC wells
+cf_model <- data.frame()
+for(i in 1:length(wellnames))
+{
+  for_in_tc <- inputs[i, 1:ncol(inputs)] ##for_in will filter the input table to for the i row
+  for_sub_tc <- subset(TCGroups, Name == wellnames[i]) ##subset to only the rows with wellname[i]
+  
+  model <- cshflow(for_sub_tc, for_in_tc) ##run cshflow function on the above
+  cf_model <- rbind(cf_model, model) ##store the results
+  
+}
+
+#-------------------for loop to calc price sensitivity
+
+
+price_model <- data.frame()
+for(i in 1:length(wellnames))
+{
+  
+  x <- data.frame(map(1:9, function(x)data.frame(Name = cf_model$TCName, 
+                                            Net.Rev = (cf_model$NetDryGas * gPrice[x] 
+                                                           + cf_model$NetOil * oPrice[x] 
+                                                           + cf_model$NetNGL * nPrice[x]))))
+  
+  
+  for_in_price <- inputs[i, 1:ncol(inputs)] ##for_in will filter the input table to for the i row
+  for_sub_price <- subset(x, Name == wellnames[i]) ##subset to only the rows with wellname[i]
+  
+  x1 <- data.frame(map(1:9, function(x)data.frame(Name = cf_model$TCName, 
+                                                 Net.Rev = (cf_model$NetDryGas * gPrice[x] 
+                                                            + cf_model$NetOil * oPrice[x] 
+                                                            + cf_model$NetNGL * nPrice[x]))))
+  
+  model1 <- 
+    ###update with new function to calc from rev to end....(for_sub, for_in) ##run cshflow function on the above
+  price_model <- rbind(price_model, model1) ##store the results
+  
+}
+
+
+
+
+
+test <- cf_model %>%
+  split(.$TCName) %>%
+  map("GRGas.mcf")
 
 
 write.csv(TCGroups, "tcgroups.csv")
 write.csv(Cashflow, "cashflow.csv")
 write.csv(result, "result.csv")
 write.csv(results, "results.csv")
-write.csv(modelout, "modelout.csv")
-
+write.csv(cf_model, "cf_model.csv")
+write.csv(purrrModel, "purrr.csv")
+write.csv(test, "test.csv")
 
 
