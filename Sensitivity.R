993891f2244fad41bb97595ef7864b702a99da46
mfarr76 <mfarr76@gmail.com>
2017-07-06 16:41:31 -0500

    fix disc sensitivity

--- Sensitivity.R
+++ Sensitivity.R
@@ -1,7 +1,7 @@
 
 rm(list = ls())
-load("C:/Users/MFARR/Documents/R_files/Spotfire.data/cashflow.RData")
-#load("C:/Users/MFARR/Documents/R_files/Spotfire.data/cashflow_v2.RData")
+#load("C:/Users/MFARR/Documents/R_files/Spotfire.data/cashflow.RData")
+load("C:/Users/MFARR/Documents/R_files/Spotfire.data/cashflow_v2.RData")
 
 library(dplyr)
 library(purrr)
@@ -164,71 +164,34 @@ if(sensitivity_choice == "GAS")
 price[2, "gPrice"]
 price[4, 1]
 
-############discount sensitivity
 
+###########-------------------------------------------
 
-cf <- function(prodMonth, capex, capexMonth, operIncome)
-{
-  #change units for capex
-  capexM <- capex*1000
-  #create discount rate range from 0 to 100%
-  discRate <- seq(0, 1, by = 0.05) 
-  #sequental list of discRate from 0 to 21
-  discRateLng <- as.numeric(seq_along(discRate))
-  
-  ##sapply function to create a matrix of discount rate and production month
-  discRateTbl <- (sapply(discRate, (function(x) 1/(1 + x)^(((prodMonth - 0.5) / 12)))))
-  ##sapply function to create a matrix of discount rate for capex
-  discCapex <- (sapply(discRate, (function(x) (1/(1 + x)^((capexMonth - 1)/12))*capexM)))
-  ##cash flow table if oper income is > 0....ie..LOSS ZERO
-  discCfTbl <- data.frame(sapply(discRateLng, function(x) ifelse((discRateTbl[,x]*operIncome)>0, 
-                                                                 (discRateTbl[,x]*operIncome), NA)))
-  ##subtract capex from cash flow table based on capex month              
-  discCfTbl[capexMonth,] <- discCfTbl[capexMonth,] - discCapex 
-  
-  ##map function from purrr to sum up the column to get a single value
-  ##could to use apply function instead
-  discCfSum <- map_df(discCfTbl, sum, na.rm = TRUE)
-  #discCfSum <- apply(discCfTbl, 2, sum)
-  ##transpose
-  discCfSum <- t(discCfSum)
-  
-  ##create a matrix called "result"
-  #result <- data.frame(matrix(0, ncol = 2, nrow = length(discRateLng)))
-  result <- data.frame(Disc.Rate = 0, NPV = discRateLng )
-  ##change the column names
-  #colnames(result) <- c("Disc.Rate", "NPV")
-  
-  ##write the results to the table
-  result$Disc.Rate <- discRate
-  result$NPV <- discCfSum
-  
-  result 
-}
-
-
-NPV.Table <- cf(Cashflow$Time, capex, capex_mnth, Cashflow$oper_income)
-
+###discount sensitivity
 
 
 
 discRate <- seq(0, 1, by = 0.05) 
-discRate[5]
+discRate[21]
 
+x <- 2
+z <- 21
+rm(x)
+rm(z)
 cshflow_discount <- function(x, z) 
   
 { 
-  red_tc <- subset(TCGroups, Name == tc_list[x]) #reduce/filter the typecurves one at a time 
-  red_inputs <- inputs[x, 1:ncol(inputs)] #reduce/filter the user inputs to match the typecurves
-  TCName = red_tc$Name
+  red_tc <- subset(CashFlow, TCName == tc_list[x]) #reduce/filter the typecurves one at a time 
+  red_inputs <- subset(inputs, wellname == tc_list[x]) #reduce/filter the user inputs to match the typecurves
+  #TCName = red_tc$Name
   Time <-  as.numeric(red_tc$Time) #
-  NetRev <- CashFlow1$GasRev + CashFlow1$OilRev + CashFlow1$NGLRev
+  NetRev <- red_tc$GasRev + red_tc$OilRev + red_tc$NGLRev
   OpIncome <- NetRev - red_inputs$opex
   Undisc.CF <- OpIncome - ifelse(Time == capex_mnth, red_inputs$capex * 1000, 0)
   Disc.Capex <- (ifelse(Time == capex_mnth, (1/(1 + discRate[z])^((capex_mnth - 1)/12))*red_inputs$capex*1000, 0))
   Disc.CF <- as.numeric(OpIncome *(1/(1 + discRate[z])^((Time - 0.5) / 12)) - Disc.Capex)
   
-  results <- data.frame(TCName, Time, Disc.CF)
+  results <- data.frame(TCName = red_tc$TCName, Time, Disc.CF)
   
   results_cshflow <- filter(results, OpIncome > 0)
   
@@ -255,4 +218,14 @@ for(i in 1:length(wellnames)){
 cashflow.dr <- Cashflow.disc %>%
   group_by(TCName, scenario) %>%
   summarise(sum(Disc.CF))
+
+
+
+
+####inerpolate
+IRR <- ifelse(as.numeric(NPV.Table[21,2]) > 0, 1,unlist(approx(NPV.Table$NPV, NPV.Table$Disc.Rate, 0))[2])
+IRR
+
+DPI <- sum(Cashflow$disc_cash_flow, na.rm = TRUE)/sum(Cashflow$disc_capex, na.rm = TRUE) + 1
+DPI
 
